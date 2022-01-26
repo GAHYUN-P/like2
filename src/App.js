876486f809ce221d32376cfcf5eb57da3aa0a133
GAHYUN-P <gahyun94526@gmail.com>
2022-01-26 15:42:49 +0900

    평점

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Home";
+import Detail from "./Detail";
+import styled from "styled-components";
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+// import { Route, Routes } from "react-router-dom"; 
+// 위에것으로 했을때는 계속 Cannot read properties of undefined (reading 'pathname') 에러가 났다.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:week_day" element={<Detail />} />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 }
 
+const Container = styled.div`
+  max-width: 400px;
+  min-height: 60vh;
+  background-color: #d2d2ff;
+  padding: 15px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 export default App;
